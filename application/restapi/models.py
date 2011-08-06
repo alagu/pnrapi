ada1c6015683cb79f2a36c6b598ee27b23c200e6
Alagu <allagappan@gmail.com>
2011-08-06 15:13:21 -0700

    Add cache wrapped getters

--- application/restapi/models.py
+++ application/restapi/models.py
@@ -2,15 +2,24 @@ from django.db import models
 from django.core.cache import cache
 
 # Create your models here.
-class Station(models.Model):
-    code = models.TextField()
-    name = models.TextField()
-    
-def get_station(code):
-  try:
-    station = Station.objects.get(code=code)
-    data = {'name':station.name, 'code' : station.code.upper()}
-  except: #DoesNotExist
-    data = {}
 
-  return data
+class StationManager(models.Manager):	
+    def get_station(self,code):
+		try:
+			station = cache.get('station_'+code,'')
+			cached = True
+			if not station:
+				cached = False
+				station = self.get(code=code)
+				cache.set('station_'+code,station)
+			
+			data = {'name':station.name.upper(), 'code':station.code.upper(),'cache':cached}
+		except:
+			data = {'name':code.upper(),'code':code.upper()}
+			
+		return data
+		
+class Station(models.Model):
+	code = models.CharField(max_length=10,primary_key=True)
+	name = models.CharField(max_length=100)
+	objects = StationManager()
