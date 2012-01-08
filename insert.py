ebbbb47d367e11e522d11e4029f72764d69e7ad2
Alagu <allagappan@gmail.com>
2012-01-08 22:14:21 +0530

    Insert script

--- insert.py
+++ insert.py
@@ -1,11 +1,11 @@
 import json
 import _mysql
 data = json.loads(open('railways_code.json').read())
-db=_mysql.connect("127.0.0.1","root","root","pnrapi")
+db=_mysql.connect("127.0.0.1","root","allagappan","pnrapi")
 for station in data['data']:
   sql = 'INSERT INTO restapi_station (`code`,`name`) values("%s","%s")' % (station['code'], station['name'])
   try :
-    sql
     db.query(sql)
+    print station['name'] + ' inserted ' 
   except :
     print '%s already inserted' % (sql)
